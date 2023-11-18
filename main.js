31369fffad9f5bf0a73613a4cb9554742aa22ac5
eeelii! <eliseocutz95@gmail.com>
2023-11-18 06:32:00 -0500

    age calculation code

--- main.js
+++ main.js
@@ -2,3 +2,10 @@ let helloWord = "hello world";
 
 console.log(helloWord.toUpperCase())
 
+const BIRTHDAY = 1995;
+
+let currentYear = 2023;
+
+let myAge = currentYear - BIRTHDAY;
+
+console.log(`Since you were born in ${BIRTHDAY} then in ${currentYear} you will be ${myAge}`)
